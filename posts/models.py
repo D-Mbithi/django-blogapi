802d6f0ccd298ca4e84e0dc9faa589a8065d41f1
dennis mbithi <denmbithi@gmail.com>
2023-02-27 19:43:16 +0300

    Added Blog model test

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from .models import Post
+
+User = get_user_model()
+
+
 # Create your tests here.
+class BlogTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username="test",
+            email="test@admin.com",
+            password="friction",
+        )
+
+        cls.post = Post.objects.create(
+            title='blog post title',
+            body='post body',
+            author=cls.user,
+        )
+
+    def test_post(self):
+        self.assertEqual(self.post.title, 'blog post title')
+        self.assertEqual(self.post.body, 'post body')
+        self.assertEqual(str(self.post.body), 'post body')
